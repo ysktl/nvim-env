849694eda5774f1fe5fc8f4a9cee5d943c78405f
ysktl <yuusky20th@gmail.com>
2026-01-17 22:28:48 +0900

    remove KCL & append Pkl

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,9 +41,6 @@ return {
 					-- Ansible
 					ansible = { "ansible-lint" },
 
-					-- KCL
-					kcl = { "kcl" },
-
 					-- HTML
 					html = { "html_beautify" },
 
@@ -322,5 +319,39 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
+  },
+
+  -- Pkl
+  {
+    "apple/pkl-neovim",
+    lazy = true,
+    ft = "pkl",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        build = function(_)
+          vim.cmd("TSUpdate")
+        end,
+      },
+      "L3MON4D3/LuaSnip",
+    },
+    build = function()
+      require('pkl-neovim').init()
+
+      -- Set up syntax highlighting.
+      vim.cmd("TSInstall pkl")
+    end,
+    config = function()
+      -- Set up snippets.
+      require("luasnip.loaders.from_snipmate").lazy_load()
+
+      -- Configure pkl-lsp
+      vim.g.pkl_neovim = {
+        start_command = { "java", "-jar", "/path/to/pkl-lsp.jar" },
+        -- or if pkl-lsp is installed with brew:
+        -- start_command = { "pkl-lsp" },
+        pkl_cli_path = "/path/to/pkl"
+      }
+    end,
   },
 }
