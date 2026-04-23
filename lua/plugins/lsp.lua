f45b8fe59f217b067cd3e51bd1972f1f1be84c8f
ysktl <yuusky20th@gmail.com>
2026-04-23 18:02:46 +0900

    fix blink depends

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -254,23 +254,10 @@ return {
 	},
 
 	{
-		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
-
+		"saghen/blink.nvim",
 		build = "cargo build --release",
+    dependencies = { "saghen/blink.lib" },
 		opts = {
-			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-			-- 'super-tab' for mappings similar to vscode (tab to accept)
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- All presets have the following mappings:
-			-- C-space: Open menu or open docs if already open
-			-- C-n/C-p or Up/Down: Select next/previous item
-			-- C-e: Hide menu
-			-- C-k: Toggle signature help (if signature.enabled = true)
-			--
-			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = { preset = "super-tab" },
 
 			appearance = {
@@ -281,6 +268,8 @@ return {
 			completion = { documentation = { auto_show = true } },
 
 			fuzzy = { implementation = "prefer_rust_with_warning" },
+      chartoggle = { enabled = true },
+      tree = { enabled = true },
 		},
 		opts_extend = { "sources.default" },
 	},
